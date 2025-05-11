edfaa14f7142d7ac5b3c3822ae7b2ec283179c7b
zaralX <61970128+zaralX@users.noreply.github.com>
2025-05-11 14:49:30 +0800

    sdk relative path

--- plugins/yandex-sdk.client.ts
+++ plugins/yandex-sdk.client.ts
@@ -1,7 +1,7 @@
 export default defineNuxtPlugin((nuxtApp) => {
     if (import.meta.client) {
         const script = document.createElement('script')
-        script.src = 'https://yandex.ru/games/sdk/v2'
+        script.src = import.meta.dev ? 'https://yandex.ru/games/sdk/v2' : '/sdk.js'
         script.async = true
         script.onload = () => {
             console.log('Yandex SDK loaded')
